d54dc718247a8d4e54d5a1fe2447633c336ba1ae
Mohammad Rafe <mrrafi5869@gmail.com>
2022-08-23 14:24:36 +0600

    players section

--- js/index.js
+++ js/index.js
@@ -1,12 +1,36 @@
 const listArray = [];
+
+function display(playerList) {
+    console.log(playerList);
+    const playersName = document.getElementById("players-name");
+    playersName.innerText = ""
+    const length = listArray.length
+    for(let i = 0; i < length; i++) {
+        
+        const name = playerList[i];
+
+        const li = document.createElement("li");
+        li.innerText = name
+
+        playersName.appendChild(li);
+        
+    }
+
+    if(length > 5){
+        alert("you can select only 5 players");
+    }
+}
+
+
 function addToList (element) {
     const parentElement = element.parentNode.children[0].innerText;
     
-    const listObj = {
-        parentElement: parentElement
-    }
+    // const listObj = {
+    //     parentElement: parentElement
+    // }
     
-    listArray.push(listObj);
-    console.log(listArray)
-    console.log(listArray.length)
+    listArray.push(parentElement);
+    display(listArray);
 }
+
+
